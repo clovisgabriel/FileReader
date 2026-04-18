006bf6e12bece1979f6f641ad6000ad2257c5bf5
Clovis Nascimento <clovisgabrielm@gmail.com>
2026-04-18 15:53:16 +0100

    Version 5: Add CLI demo covering all versions and testing encrypted and secured XML reader

---
+++ FileReader.Cli/Program.cs
@@ -0,0 +1,102 @@
+﻿using FileReader.Core;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("###### File Reader ######\n\n");
+
+        // Get base folder of running application
+        var basePath = AppContext.BaseDirectory;
+
+        // Sample files
+        string textFile = Path.Combine(basePath, "Files", "textFile.txt");
+        string xmlFile = Path.Combine(basePath, "Files", "xmlFile.xml");
+        string encryptedFile = Path.Combine(basePath, "Files", "encryptedFile.txt");
+
+        Console.WriteLine("1. TEXT FILE READING (v1)");
+        TestText(textFile);
+
+        Console.WriteLine("\n----------------------------------------------\n");
+
+        Console.WriteLine("2. XML FILE READING (v2)");
+        TestXml(xmlFile);
+
+        Console.WriteLine("\n----------------------------------------------\n");
+
+        Console.WriteLine("3. ENCRYPTED TEXT FILE (v3)");
+        TestEncryptedText(encryptedFile);
+
+        Console.WriteLine("\n----------------------------------------------\n");
+
+        Console.WriteLine("4. XML + SECURITY (AUTHORIZED, USER ROLE = ADMIN) (v4)");
+        TestXmlWithSecurity(xmlFile);
+
+        Console.WriteLine("\n----------------------------------------------\n");
+
+        Console.WriteLine("5. XML + ENCRYPTION + SECURITY (AUTHORIZED, USER ROLE = ADMIN) (v5)");
+        TestXmlEncryptedWithSecurity(xmlFile);
+
+        Console.WriteLine("\n----------------------------------------------\n");
+
+    }
+
+    // V1 - Test a simple text file reader
+    static void TestText(string path)
+    {
+        IFileReader reader = new TextFileReader();
+        var result = reader.Read(path);
+
+        Console.WriteLine(result);
+    }
+
+    // V2 - Test an xml file reader
+    static void TestXml(string path)
+    {
+        IFileReader reader = new XmlFileReader();
+        var result = reader.Read(path);
+
+        Console.WriteLine(result);
+    }
+
+    // v3 - Text an encrypted text file reader
+    static void TestEncryptedText(string path)
+    {
+        IFileReader reader =
+            new EncryptedFileReader(
+                new TextFileReader(),
+                new ReverseEncryptionStrategy());
+
+        var result = reader.Read(path);
+
+        Console.WriteLine(result);
+    }
+
+    // V4 - Test an xml with security
+    static void TestXmlWithSecurity(string path)
+    {
+        IFileReader reader =
+            new SecureXmlFileReader(
+                new XmlFileReader(),
+                UserRole.Admin); // Change UserRole to see unauthorized access exception. 
+
+        var result = reader.Read(path);
+
+        Console.WriteLine(result);
+    }
+
+    // V5 - Test an xml encrypted file with security
+    static void TestXmlEncryptedWithSecurity(string path)
+    {
+        IFileReader reader =
+            new SecureXmlFileReader(
+                new EncryptedFileReader(
+                    new XmlFileReader(),
+                    new ReverseEncryptionStrategy()),
+                UserRole.Admin);
+
+        var result = reader.Read(path);
+
+        Console.WriteLine(result);
+    }
+}
